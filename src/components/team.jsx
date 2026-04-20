4c8fe77bc2c0c1f220499c70ce3fb0b50e942b60
Lina Saidi <saidilina652@gmail.com>
2026-04-20 23:47:41 +0200

    fix

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -6,67 +6,67 @@ const data = [
     name: "Ramy Zoubiri",
     age : "23 ans",
     achivement: "Founder of Vexa agence and content creator",
-    image: "/ramyy.jpg" 
+    image: "https://res.cloudinary.com/dfxbxle3g/image/upload/f_auto,q_auto,w_250/v1776720893/ramyy_jbwsu8.jpg" 
   },
   {
     name: "Lina Saidi",
     age : "21 ans",
     achivement: "websites creator and content creator",
-   image: "/linaa.jpg" 
+   image: "https://res.cloudinary.com/dfxbxle3g/image/upload/f_auto,q_auto,w_250/v1776720883/photo_2026-03-12_23-37-35_piq9mi.jpg" 
   },
   {
     name: "Malak Ben Machiche",
     age: "24 ans",
-    achivement: "Manager and UGC content creator",
-    image: "/malaak.jpg"
+    achivement: "Manager & UGC content creator",
+    image: "https://res.cloudinary.com/dfxbxle3g/image/upload/f_auto,q_auto,w_250/v1776720844/malaak_i2pdjc.jpg"
   }
   ,{
-    name: "Yassine Tifratene",
+    name: "Yasmine Tifratene",
     age: "22 ans",
     achivement: "UGC content creator",
-    image: "/yasmine.jpg"
+    image: "https://res.cloudinary.com/dfxbxle3g/image/upload/w_400,f_auto,q_auto/v1776720032/yasmine_xba2lk.jpg"
   }
   ,{
     name: "Embouazza chaimae ",
     age: "21 ans",
     achivement: "designer and content creator",
-    image: "/chaima.jpg"
+    image: "https://res.cloudinary.com/dfxbxle3g/image/upload/v1776719406/CHAIMA_yc2n6j.jpg"
   },
   {
     name: "Meliza Bouhidel",
     age : "20 ans",
     achivement: "Content creator - Community Manager",
-    image: "/melizaa.PNG" 
+    image: "https://res.cloudinary.com/dfxbxle3g/image/upload/f_auto,q_auto,w_250/v1776720863/melizaa_o90h2t.jpg" 
   },
   {
     name: "Oussama Hamdaoui",
     age : "24 ans",
     achivement: "Co-fondateur - Éditeur",
-   image: "/oussama.JPG" 
+   image: "https://res.cloudinary.com/dfxbxle3g/image/upload/f_auto,q_auto,w_250/v1776720160/oussama_crie6t.jpg"
   },
   {
     name: "Maria Boukhalfa",
     age: "20 ans",
     achivement: "Media face",
-    image: "/maria.jpg"
+    image: "https://res.cloudinary.com/dfxbxle3g/image/upload/f_auto,q_auto,w_250/v1776720171/MARIA_rupndr.jpg"
   }
   ,{
     name: "Amel Allaoua",
     age: "23 ans",
     achivement: " content creator",
-    image: "/amel.PNG"
+    image: "https://res.cloudinary.com/dfxbxle3g/image/upload/f_auto,q_auto,w_250/v1776720827/amel_m1glpv.jpg"
   }
   ,{
     name: "Dhikra Farhtia ",
     age: "20 ans",
     achivement: "UGC content creator",
-    image: "/dhikra.PNG"
+    image: "https://res.cloudinary.com/dfxbxle3g/image/upload/f_auto,q_auto,w_250/v1776720181/dhikra_vazuar.jpg"
   }
    ,{
     name: "Meriem Abdallah ",
     age: "20 ans",
     achivement: "Editor",
-    image: "/meriem.png"
+    image: "https://res.cloudinary.com/dfxbxle3g/image/upload/f_auto,q_auto,w_250/v1776721392/meriem_azy1eg.jpg"
   }
 
 ];
